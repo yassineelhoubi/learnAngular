8ac5c2a29659be0ed73ed7efa95b4a688ebd237e
yassineelhoubi <elhoubiyassine@gmail.com>
2021-09-02 16:28:08 +0100

    make the navigating to hero details with url

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,16 +11,13 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   selectedHero ?: Hero ;
   heroes: Hero[] = [];
-  constructor(private heroService: HeroService , private messageService: MessageService) {}
+  constructor(private heroService: HeroService ) {}
 
   ngOnInit(): void {
     this.getHeroes()
   }
   
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes)
