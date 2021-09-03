dbc4dbf723e2beb2a597a6415edc9d86ec0b5a36
yassineelhoubi <elhoubiyassine@gmail.com>
2021-09-03 11:25:31 +0100

    add a new data

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -18,7 +18,7 @@ export class HeroService {
     private messageService: MessageService) { }
 
   private heroesUrl = 'api/heroes';  // URL to web api
-  
+
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
@@ -82,10 +82,18 @@ export class HeroService {
   }
 
   /** PUT: update the hero on the server */
-updateHero(hero: Hero): Observable<any> {
-  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-    tap(_ => this.log(`updated hero id=${hero.id}`)),
-    catchError(this.handleError<any>('updateHero'))
-  );
-}
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -24,4 +24,13 @@ export class HeroesComponent implements OnInit {
     
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
 }
